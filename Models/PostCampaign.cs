b69a036dcf9c87b8f870b2b7cec5915e0acf9fca
aeongt710 <unknowndonlion@gmail.com>
2022-12-01 12:35:04 +0500

    before post campaign

---
+++ Models/PostCampaign.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace FYP1.Models
+{
+    public class PostCampaign
+    {
+        [Required]
+        public string Title{ get; set; }
+        [Required]
+        [Range(0.1,10)]
+        public double Amount { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public IBrowserFile ImgFile{ get; set; }
+
+
+        public string? DescHash { get; set; }
+        public string? ImgHash { get; set; }
+
+        public Guid ImgClassGuid { get; set; } = Guid.NewGuid();
+
+    }
+}
